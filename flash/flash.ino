68e1d2d60964f24d41bbc463d5881c42ad3ccee9
anhhao135 <anhhao135@gmail.com>
2020-12-15 02:33:44 -0800

    new stuff

--- flash/flash.ino
+++ flash/flash.ino
@@ -1,7 +1,14 @@
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);// Initialize the LED_BUILTIN pin as an output
+  pinMode(14, INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly
+  delay(50);
+  Serial.println(analogRead(A0));
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(50);
+  digitalWrite(LED_BUILTIN, HIGH);
+  Serial.println(digitalRead(14));
 }
